9b79d431fdab4604ea0ea005ddbb351630841078
vagnermengali <vagmengali2015@gmail.com>
2024-03-12 22:26:29 -0300

    feat: add new qa

--- src/components/Other/LineScroll/index.tsx
+++ src/components/Other/LineScroll/index.tsx
@@ -37,9 +37,9 @@ const LineScroll = () => {
 
   return (
     <>
-      <div className="flex w-fit flex-col items-center justify-center fixed left-auto right-[6.25%] -bottom-[30px] sm:right-10 lg:left-10 lg:bottom-0 z-30">
+      <div className="flex w-fit flex-col items-center justify-center fixed left-auto right-5 -bottom-8 sm:right-10 lg:left-10 lg:bottom-0 z-30">
         <motion.p
-          className="relative whitespace-nowrap uppercase text-white z-50 font-medium tracking-[0.2rem] text-[.625rem] [writing-mode:vertical-lr] sm:text-xs lg:text-sm after:content-[''] after:w-[.125rem] after:h-16 after:bg-white after:absolute after:left-0 after:right-0 after:m-auto after:-bottom-20"
+          className="relative whitespace-nowrap uppercase text-white z-50 font-medium tracking-down text-xs [writing-mode:vertical-lr] lg:text-sm after:content-[''] after:w-1 after:h-16 after:bg-white after:absolute after:left-0 after:right-0 after:m-auto after:-bottom-20"
           initial={{ bottom: scroll ? "5rem" : "-11rem" }}
           animate={{ bottom: scroll ? "-11rem" : "5rem" }}
         >
