85a9ccb6fbda90e2d68a2ecba2f1882e1179b9cf
vagnermengali <vagmengali2015@gmail.com>
2024-03-12 22:35:23 -0300

    feat: add new qa

--- src/components/Other/SkillSet/index.tsx
+++ src/components/Other/SkillSet/index.tsx
@@ -6,8 +6,8 @@ import { SkillData } from "@/interfaces/SkillProcessProps/SkillProcessProps";
 
 const SkillSet = () => {
   return (
-    <div className="w-full h-fit sm:h-full flex items-center justify-start lg:justify-center flex-col pt-[5.3125rem] pb-10 sm:py-20 lg:py-[6.25rem] ">
-      <ul className="w-[83%] h-3/4 lg:h-full flex flex-col gap-[0.625rem] sm:gap-5 lg:grid-cols-2 lg:gap-x-10 lg:gap-y-10 lg:grid lg:flex-row lg:items-center lg:justify-center">
+    <div className="w-full h-fit sm:h-full flex items-center justify-start lg:justify-center flex-col pt-20 pb-10 sm:py-20 lg:py-24">
+      <ul className="w-full h-3/4 lg:h-full flex flex-col gap-2 sm:gap-5 lg:grid-cols-2 lg:gap-x-10 lg:gap-y-10 lg:grid lg:flex-row lg:items-center lg:justify-center px-5 sm:px-10">
         {Skill.map((e: SkillData) => (
           <SkillProcess
             key={e.id}
