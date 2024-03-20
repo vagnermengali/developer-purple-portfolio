76512ded2eb7172d07575ef0d5c595dae4752f04
vagnermengali <vagmengali2015@gmail.com>
2024-03-20 19:12:31 -0300

    feat: add new limit screen

--- src/components/Other/DarkDiv/index.tsx
+++ src/components/Other/DarkDiv/index.tsx
@@ -5,7 +5,7 @@ import { TransitionVariants } from "@/components/Animations/Transition";
 const Transition = () => {
 
     return (
-        <motion.div className="fixed top-0 w-screen h-dvh bottom-full z-50 transition-all duration-300 ease-linear bg-black" variants={TransitionVariants}
+        <motion.div className="fixed top-0 w-screen h-dvh bottom-full z-50 transition-all duration-200 ease-linear bg-gradient2" variants={TransitionVariants}
             initial="initial"
             animate="animate"
             exit="exit"
