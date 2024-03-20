672073bdd0e5354576efc93921a2a28820870f90
vagnermengali <vagmengali2015@gmail.com>
2024-03-20 19:25:27 -0300

    feat: add new limit screen

--- src/components/Animations/Transition.tsx
+++ src/components/Animations/Transition.tsx
@@ -1,4 +1,4 @@
-export const TransitionVariants = {
+/* export const TransitionVariants = {
     initial: {
         height: "100%",
     },
@@ -7,5 +7,24 @@ export const TransitionVariants = {
     },
     exit: {
         height: ["0%", "100%"],
+    },
+}; */
+
+
+export const TransitionVariants = {
+    initial: {
+        translateY: "0%",
+    },
+    animate: {
+        translateY: "-100%",
+        transition: {
+            duration: 0.3,
+        },
+    },
+    exit: {
+        translateY: "0%",
+        transition: {
+            duration: 0.3,
+        },
     },
 };
