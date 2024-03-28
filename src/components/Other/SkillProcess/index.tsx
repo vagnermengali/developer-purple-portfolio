c1d4fc851768f4ea42a4012218172e5678e78365
vagnermengali <vagmengali2015@gmail.com>
2024-03-28 01:33:30 -0300

    feat: add new qa work

--- src/components/Other/SkillProcess/index.tsx
+++ src/components/Other/SkillProcess/index.tsx
@@ -10,16 +10,16 @@ const SkillProcess = ({ title, years, percentage }: SkillProcessProps) => {
         <Icon className="relative w-full" id={title} />
       </div>
       <div className="w-full h-full gap-3 flex justify-center flex-col">
-        <div className="flex items-center justify-between gap-5">
+        <div className="flex items-end justify-between gap-5">
           <div className="flex items-end justify-center gap-1">
             <h2 className="text-sm max-w-24 sm:text-xl text-brand9 leading-tight whitespace-nowrap tracking-down font-bold">{title}</h2>
-            <p className="w-0 overflow-hidden text-brand5 tracking-down leading-tight text-xs sm:text-sm">{years}</p>
+            <p className="w-0 overflow-hidden leading-none">{years}</p> {/* text-brand5 tracking-down leading-tight text-xs sm:text-sm */}
           </div>
-          <p className="text-2xl text-brand1 tracking-down leading-tight font-bold">
-            {percentage}<span className="text-brand4 tracking-down leading-tight text-xs sm:text-base font-light">%</span>
+          <p className="text-2xl text-brand1 tracking-down leading-none font-bold">
+            {percentage}<span className="text-brand4 tracking-down leading-none text-xs sm:text-base font-light">%</span>
           </p>
         </div>
-        <div className="w-full relative">
+        <div className="w-full relative h-3">
           <div className={`w-full h-2 sm:h-3 absolute rounded-xl bg-blur`}></div>
           <div className={`w-full h-2 sm:h-3 absolute rounded-xl bg-gradient1`} style={{ width: `${percentage.toString()}%` }}></div>
         </div>
